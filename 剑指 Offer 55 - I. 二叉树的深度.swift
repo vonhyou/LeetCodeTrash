93495eeac9c1bf2c95e3078c91a8f174fad349b0
vonhyou <vonhyou@yeah.net>
2020-11-26 20:09:00 +0800

    add binary tree

---
+++ 剑指 Offer 55 - I. 二叉树的深度.swift
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class Solution {
+  func maxDepth(_ root: TreeNode?) -> Int {
+    if root == nil {
+      return 0
+    } else {
+      return max(maxDepth(root?.left), maxDepth(root: TreeNode?)) + 1
+    }
+  }
+}
